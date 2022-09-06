6486defe9426d4d98c25ec0a2df4a0103cff9e5e
sukmaarifwibowo <112854007+sukmaarifwibowo@users.noreply.github.com>
2022-09-06 07:16:45 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Sukma Arif Wibowo" << endl;
+    cout << "SUKMA ARIF WIBOWO, A11.2022.14144, BEKASI, SMK TAMAN HARAPAN" << endl;
+    return 0;
+}
