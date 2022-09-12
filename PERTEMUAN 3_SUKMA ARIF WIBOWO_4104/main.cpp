0cfe0eaa096f56a5103b979e72a3b0a63f63940a
sukmaarifwibowo <112854007+sukmaarifwibowo@users.noreply.github.com>
2022-09-12 14:45:31 +0700

    Add files via upload

---
+++ PERTEMUAN 3_SUKMA ARIF WIBOWO_4104/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    //integer
+    int umur = 18;
+    float t = 165.5;
+    char b = 'XL';
+    string UB = "XL";
+
+    cout << "Umur" << umur;
+    cout << endl;
+    cout << "Tinggi Badan" << t;
+    cout << "\nUkuran Baju" << UB;
+
+
+    return 0;
+}
