2901305e7f7d1bfd03fb27fb0248160bc054af32
sukmaarifwibowo <112854007+sukmaarifwibowo@users.noreply.github.com>
2022-11-26 17:20:29 +0700

    Add files via upload

---
+++ TUGAS PROBLEM PDP_SUKMA ARIF WIBOWO_14144_4104/TUGAS NO. 10_SUKMA ARIF WIBOWO_14144_4104/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+
+    for (int x = 1; x <= 10; x++) {
+        cout << "\nLooping Yang Ke - " << x << endl;
+        for (int y = 1; y <= 10; y++) {
+        cout << "Angka " << y << endl;
+    }
+    }
+    return 0;
+}
